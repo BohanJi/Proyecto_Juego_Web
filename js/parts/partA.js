35a66d4cb95c7ce3bb6699a5075bc230965869e0
Paula Herranz Gómez <paulaherranzgomez@gmail.com>
2022-04-28 17:43:31 +0200

    aaaaaaaaaaaaaa

--- js/parts/partA.js
+++ js/parts/partA.js
@@ -1,12 +1,13 @@
 const HUD_HEIGHT = 80;
+const TIMER_RHYTHM=0.1*Phaser.Timer.SECOND;
+const PRODUCTOS_GROUP_SIZE = 200;
 
 
 
-let dwords;
 
 let background;
 
-let words;
+let letters;
 
 let carrito;
 
@@ -20,9 +21,22 @@ let partAState = {
 
 function preloadPartAState () {
     game.load.image('background',
-    'assets/imgs/supermercado.jpg');
+        'assets/imgs/supermercado.jpg');
+
     game.load.image('carrito',
-    'assets/imgs/carrito.png');
+        'assets/imgs/carrito.png');
+
+    game.load.image('fruta',
+        'assets/imgs/fruta.png');
+
+    game.load.image('botella',
+        'assets/imgs/ufo.png');
+
+    game.load.image('brick',
+        'assets/imgs/brick.png');
+
+    game.load.image('verdura',
+        'assets/imgs/verdura.png');
 };
 
 function createPartAState () {
@@ -34,9 +48,8 @@ function createPartAState () {
     createCarrito();
     createHUD();
 
-    words = game.add.group();
-    words.inputEnableChildren = true;
-    dwords=game.add.group();
+    letters = game.add.group();
+    letters.inputEnableChildren = true;
     game.input.keyboard.onDownCallback = getKeyboardInput;
 
 };
@@ -48,13 +61,11 @@ function updatePartAState () {
 
 function getKeyboardInput(e) {
     if (e.keyCode >= Phaser.Keyboard.A && e.keyCode <= Phaser.Keyboard.Z) {
-        let a = game.add.text(Math.random() * game.width, Math.random() * game.height, e.key, {fontSize: '40px', fill: '#FA2'}, words); // group to add to
+        let a = game.add.text(Math.random() * game.width, Math.random() * game.height, e.key, {fontSize: '40px', fill: '#FA2'}, letters); // group to add to
         a.anchor.setTo(0.5, 0.5);
     }
 };
-function displaywords(){
-    
-}
+
 
 function createCarrito() {
     let x = game.world.centerX;
@@ -64,4 +75,22 @@ function createCarrito() {
     carrito.scale.setTo(0.3);
     game.physics.arcade.enable(carrito);
     //carrito.body.collideWorldBounds = true;
+}
+
+function createProductos(number) {
+    productos = game.add.group();
+    productos.enableBody = true;
+    productos.createMultiple(number, 'ufo');
+    productos.callAll('events.onOutOfBounds.add',
+        'events.onOutOfBounds', resetMember);
+    productos.callAll('anchor.setTo', 'anchor', 0.5, 1.0);
+    productos.setAll('checkWorldBounds', true);
+    currentProductoProbability = 0.2;
+    currentProductoVelocity = 50;
+    game.time.events.loop(
+        TIMER_RHYTHM, activateProducto, this);
+    currentProductoProbability =
+        LEVEL_PRODUCTO_PROBABILITY[level-1];
+    currentProductoVelocity =
+        LEVEL_PRODUCTO_VELOCITY[level-1];
 }
