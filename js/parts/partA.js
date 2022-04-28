c3595041f9c119dac6e245041488df043f72d87e
al405059 <92725828+al405059@users.noreply.github.com>
2022-04-28 16:27:02 +0100

    Update partA.js

--- js/parts/partA.js
+++ js/parts/partA.js
@@ -1,4 +1,5 @@
 let background;
+let letters;
 let partAState = {
     preload: preloadPartAState,
     create: createPartAState,
@@ -17,10 +18,21 @@ function createPartAState () {
         0, 0, w, h, 'background');
 
     createHUD();
+    letters = game.add.group();
+    letters.inputEnableChildren = true;
+    game.input.keyboard.onDownCallback = getKeyboardInput;
+    
 };
 
 function updatePartAState () {
     
     background.tilePosition.y += 1;
     
+};
+
+function getKeyboardInput(e) {
+    if (e.keyCode >= Phaser.Keyboard.A && e.keyCode <= Phaser.Keyboard.Z) {
+        let a = game.add.text(Math.random() * game.width, Math.random() * game.height, e.key, {fontSize: '40px', fill: '#FA2'}, letters); // group to add to
+        a.anchor.setTo(0.5, 0.5);
+    }
 };
