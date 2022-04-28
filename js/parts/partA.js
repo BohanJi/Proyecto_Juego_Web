e8d63ac1ba609f240c2d5dfde8216c53f845a17d
Paula Herranz Gómez <paulaherranzgomez@gmail.com>
2022-04-28 17:22:30 +0200

    añado fotos de comida

--- js/parts/partA.js
+++ js/parts/partA.js
@@ -1,4 +1,13 @@
+const HUD_HEIGHT = 80;
+
+
+
+
+
 let background;
+let carrito;
+
+
 let partAState = {
     preload: preloadPartAState,
     create: createPartAState,
@@ -8,6 +17,8 @@ let partAState = {
 function preloadPartAState () {
     game.load.image('background',
     'assets/imgs/supermercado.jpg');
+    game.load.image('carrito',
+    'assets/imgs/carrito.png');
 };
 
 function createPartAState () {
@@ -15,6 +26,7 @@ function createPartAState () {
     let h = game.world.height;
     background = game.add.tileSprite(
         0, 0, w, h, 'background');
+    createCarrito();
 };
 
 function updatePartAState () {
@@ -22,3 +34,14 @@ function updatePartAState () {
     background.tilePosition.y += 1;
     
 };
+
+
+function createCarrito() {
+    let x = game.world.centerX;
+    let y = game.world.height - HUD_HEIGHT;
+    carrito = game.add.sprite(x, y, 'carrito');
+    carrito.anchor.setTo(0.5, 0.5);
+    carrito.scale.setTo(0.3);
+    game.physics.arcade.enable(carrito);
+    //carrito.body.collideWorldBounds = true;
+}
