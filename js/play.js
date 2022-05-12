cf4d2a2537740d4d0c32b6d5f9e313942cd560af
al405059 <92725828+al405059@users.noreply.github.com>
2022-05-12 15:36:19 +0100

    Update play.js

--- js/play.js
+++ js/play.js
@@ -86,6 +86,6 @@ function carritoRender(){
 
 function moveProduct(producto,velocity){
     //direccion
-    game.moveToObject(producto,carrito);
+    producto.rotation = this.game.physics.arcade.moveToObject(producto, this.target, velocity)
+ 
 }
-
